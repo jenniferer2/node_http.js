3ca23e17338615bb65093836c35fb738db094172
jenniferer2 <98603685+jenniferer2@users.noreply.github.com>
2023-01-23 13:32:03 -0500

    Add files via upload

--- new.js
+++ new.js
@@ -1,5 +1,6 @@
 const express = require ('express');
 const app = express(); 
+app.use(express.json());
 app.get('/', (req,res)=> {
     res.send('Hello There');
 });
@@ -10,10 +11,27 @@ const courses = [
     {id:3, name: 'Cybersecurity'},
 ];
 
+
+
 app.get('/api/courses', (req,res)=>{
     res.send(courses);
 });
+app.post('/api/courses', (req,res) => {
 
+    const courseL = courses.find(c => c.id === parseInt(req.params.id)).length();
+    if (courseL.min(3)) {
+        const course ={
+            //we assign an ID and a name property
+            id: courses.length +1,
+            name:req.body.name
+    }
+    course.
+
+    }
+    
+    
+    
+    });
 app.get('/api/courses/:id',(req,res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
     if (!course) {
